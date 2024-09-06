339a99c24082d024ba8aa2db133d289fb70a85ec
Bjørn Tore Kopperud <kopperud@protonmail.com>
2024-09-06 11:16:33 +0200

    some typos

--- scripts/array_job_launch.sh
+++ scripts/array_job_launch.sh
@@ -1,4 +1,4 @@
 #-- Launch slurm job array
 echo -e "Launching analyses... "
-sbatch -p krypton --array="1-$(wc -l scripts/arg_list_scm.txt | cut -d ' ' -f1)" ./scripts/job_scm.sh
+sbatch -p krypton --array="1-$(wc -l scripts/arg_list.txt | cut -d ' ' -f1)" ./scripts/job_scm.sh
 echo "done"
